b593fc9a29610cdefd2d30864fce6542927f69b9
BilionBoy <dhiogoandre26@gmail.com>
2025-01-14 22:15:46 -0400

    refactor: estilizacao e espacamento inputs de login

--- components/ui/LoginForm.tsx
+++ components/ui/LoginForm.tsx
@@ -5,7 +5,7 @@ const LoginForm = () => {
         <div>
 
             {/* Formulário de Login */}
-            <div className="space-y-6 mt-6">
+            <div className="space-y-3 mt-6">
                 <Input
                     id="email"
                     name="email"
@@ -24,20 +24,21 @@ const LoginForm = () => {
                     required
                 />
 
-                <button className="w-full px-4 py-3 text-base font-bold text-white bg-green-500 rounded-full hover:bg-green-600">
+                <button className="w-full px-4 py-3 text-base font-bold text-black bg-green-500 rounded-full hover:bg-green-600">
                     Entrar
                 </button>
                 <div className="flex flex-col items-center mt-4 space-y-2">
-                    <a href="#" className="text-sm text-gray-400 hover:underline">
+                    <a href="#" className="text-sm text-white underline">
                         Esqueceu sua senha?
                     </a>
                     <p className="text-sm text-gray-400">
                         Não tem uma conta?{' '}
-                        <a href="#" className="font-medium text-white hover:underline">
+                        <a href="#" className="font-medium text-white hover:no-underline">
                             Inscrever-se no Spotify
                         </a>
                     </p>
                 </div>
+
             </div>
 
         </div>
