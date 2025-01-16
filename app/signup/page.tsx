e259adcb4ec0f28408d98a959c42b22cdbfb8159
BilionBoy <dhiogoandre26@gmail.com>
2025-01-15 21:10:57 -0400

    refactor: background

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -3,12 +3,14 @@ import FacebookCadastro from '@/components/ui/logos/FacebookCadastro';
 import AppleCadastro from '@/components/ui/logos/AppleCadastro';
 import SpotifyLogo from '@/components/ui/logos/SpotifyLogo';
 import Input from '@/components/ui/input';
-
 export default function SignupPage() {
     return (
-        <div className="flex flex-col items-center justify-start min-h-screen bg-gradient-to-b from-black to-neutral-900 pt-10">
+        <div className="flex flex-col items-center justify-start min-h-screen pt-10 relative">
+            {/* Fundo com gradiente mais preto */}
+            <div className="absolute inset-0 bg-gradient-to-b from-[#000000] via-[#0c0c0c] to-[#000000] opacity-98"></div>
+
             {/* Logo e Título */}
-            <div className="flex flex-col items-center mb-8">
+            <div className="flex flex-col items-center mb-8 z-10 relative">
                 <div className="mb-6">
                     <SpotifyLogo />
                 </div>
@@ -21,14 +23,10 @@ export default function SignupPage() {
                     <span className="block">Se inscreva e</span>
                     <span className="block">comece a curtir</span>
                 </h1>
-
-
-
-
             </div>
 
             {/* Formulário */}
-            <div className="w-full max-w-sm px-6">
+            <div className="w-full max-w-sm px-6 z-10 relative">
                 <Input
                     id="email"
                     name="email"
@@ -70,7 +68,7 @@ export default function SignupPage() {
             </div>
 
             {/* Rodapé */}
-            <div className="mt-8 text-center text-sm text-gray-400">
+            <div className="mt-8 text-center text-sm text-gray-400 z-10 relative">
                 Já tem uma conta?{' '}
                 <a href="login" className="text-white hover:underline">
                     Faça login aqui.
