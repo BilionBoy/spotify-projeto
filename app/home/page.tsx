0f649c44ed07460b80b6c953bc8920a6df8c7c46
BilionBoy <dhiogoandre26@gmail.com>
2025-01-16 01:41:19 -0400

    "refactor: use client

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,5 +1,7 @@
-import Sidebar from "../../components/ui/home/Sidebar";
-import Player from "../../components/ui/home/Player";
+"use client";
+
+import Sidebar from "@/components/ui/home/Sidebar";
+import Player from "@/components/ui/home/Player";
 
 export default function HomePage() {
     return (
@@ -12,6 +14,7 @@ export default function HomePage() {
                 {/* Main Content Area */}
                 <main className="flex-1 p-6 bg-gray-900 text-white">
                     <h1 className="text-2xl font-bold">Bem-vindo ao Spotify</h1>
+                    {/* Outras seções da home podem ser adicionadas aqui */}
                 </main>
 
                 {/* Player */}
